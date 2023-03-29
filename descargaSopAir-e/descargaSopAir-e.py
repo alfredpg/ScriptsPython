7165e04538e9a0dbba2054c4ac79097a3ba8ccef
Alfredo Padilla <doalfrepg@gmail.com>
2023-03-29 12:37:50 -0500

    actualizar

---
+++ descargaSopAir-e/descargaSopAir-e.py
@@ -0,0 +1,16 @@
+# import dask
+import pandas as pd
+# import dask.dataframe as dd
+# import numpy as np
+# from pandas import ExcelWriter
+# from fConsolidarClientes import DistanciaCoord
+# import tkinter as tk
+from tkinter import filedialog
+# from tkinter import messagebox
+# from xlwt import Workbook
+# import xlwt
+import time
+import sys
+import warnings
+
+
