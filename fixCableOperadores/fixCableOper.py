dec337c470de3597aa2c0532acaf04d4249ab8b8
Alfredo Padilla <111400416+alfredpg@users.noreply.github.com>
2024-03-06 21:51:29 -0500

    ajuste
    se quitan 3 columnas finales, se ajusta el rango de inicio de la tabla

--- fixCableOperadores/fixCableOper.py
+++ fixCableOperadores/fixCableOper.py
@@ -25,7 +25,7 @@ import warnings
 # CONSTANTES
 # =============================================================================
 
-nombreExcel = "MAESTRO_VIABILIDADES.xlsx"
+nombreExcel = "VIABILIDADES2.xlsx"
 numero_iteraciones = 10
 
 # =============================================================================
@@ -36,13 +36,13 @@ lista_df_temp = []
 # =============================================================================
 # Cargue de BBDD
 # =============================================================================
-file_path = filedialog.askopenfilename(defaultextension=".xls")
-if file_path is None:
-    # quit()
-    sys.exit()
-df = pd.read_excel(file_path, header=3)
-#df = pd.read_excel(nombreExcel, header=3)
-df = df.drop(columns = 'Unnamed: 0')
+# file_path = filedialog.askopenfilename(defaultextension=".xls")
+# if file_path is None:
+#     # quit()
+#     sys.exit()
+# df = pd.read_excel(file_path, header=3)
+df = pd.read_excel(nombreExcel)
+#df = df.drop(columns = 'Unnamed: 0')
 print("Archivo cargado correctamente con "+str(len(df))+" filas \n")
 
 # =============================================================================
@@ -53,7 +53,7 @@ warnings.warn('DelftStack')
 warnings.warn('Do not show this message')
 
 primeras_columnas = df.iloc[:, :16]
-ultimas_columnas = df.iloc[:, -6:]
+ultimas_columnas = df.iloc[:, -3:]
 df_general = pd.concat([primeras_columnas, ultimas_columnas], axis=1)
 
 numColumnasInicio = 16
@@ -80,8 +80,8 @@ df_final.drop(df_finalNull.index , inplace=True)
 print("se eliminaron "+str(len(df_finalNull))+ " Null nan")
 print("OK conv1 : "+str(len(df_final)))
 
-id_vars = df_final.iloc[:, :24].columns
-value_vars = df_final.iloc[:,24:-1].columns
+id_vars = df_final.iloc[:, :21].columns
+value_vars = df_final.iloc[:,21:-1].columns
 
 df_melted = df_final.melt(id_vars=id_vars, value_vars=value_vars, var_name='tipo_elemento', value_name='cantidad')
 print("\ncantidad conv2 : "+str(len(df_melted)))
@@ -89,7 +89,7 @@ df_melted0 = df_melted[df_melted["cantidad"] == 0]
 df_melted.drop(df_melted0.index , inplace=True)
 print("se eliminaron "+str(len(df_melted0))+ " ceros (0)")
 print("OK conv2 : "+str(len(df_melted)))
-
+"""
 root = tk.Tk()
 root.withdraw() 
 
@@ -100,11 +100,9 @@ if nombre_archivo:
     df_melted.to_excel(nombre_archivo, index=False)
     print("DataFrame guardado exitosamente como:", nombre_archivo)
 
-
-
-
-
-
+"""
+df_melted.to_excel("result.xlsx", index=False)
+print("DataFrame guardado exitosamente como:","result.xlsx")
 
 
 
